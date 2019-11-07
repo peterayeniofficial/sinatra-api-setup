054bdd80bd14f603674a36c94e0e3bf1b7ac9e8f
Peter Ayeni <ayenicology@gmail.com>
2019-11-07 16:17:06 +0000

    fix relationship

--- lib/models/review.rb
+++ lib/models/review.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Review < ActiveRecord::Base
-  belongs_to :user, class_name: "user", foreign_key: "user_id"
+  belongs_to :user
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  has_many :reviews, class_name: "review", foreign_key: "reference_id"
+  has_many :reviews
 end
